8288cb4731c298ef53354a42a8c38b1f715d637e
Yvan <yvankiller92@gmail.com>
2019-11-23 17:35:52 +0100

    Classe Joueur, Element, Piege

---
+++ Assets/Scripts/Piege.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piege : MonoBehaviour
+{
+
+    public float peur;
+    public float vitesseSortie;
+
+    private bool actif = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(false); // transforme.PrendreEnfant(1).jeuObjet.mettreActif(FAUX);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnMouseDown()
+    {
+        if (actif)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(true);
+            actif = false;
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(actif){
+            other.GetComponent<Joueur>().UtilisationPiege(vitesseSortie, peur);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/SubElement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubElement : MonoBehaviour
+{
+    public bool instacie;
+    public GameObject piege;
+    public GameObject joueur;
+    public float peur = 0f;
+    public float vitesse = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instacie)
+        {
+            Instantiate(piege, new Vector3(transform.position.x,0), transform.rotation);
+        }
+        joueur.GetComponent<Joueur>().UtilisationElement(vitesse, peur, transform.position.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
